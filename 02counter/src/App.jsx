70346c1d1c1780f47375d753a0eb6b2808adf6a3
Jay Butani <jaybutani606@gmail.com>
2024-08-17 21:27:43 +0530

    second commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,27 +1,14 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-=======
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
->>>>>>> c8978ecd185e7aaf4e007194e23f653dc2292b57
 import './App.css'
 
 function App() {
 
-<<<<<<< HEAD
   const [counter, setcounter] = useState(15) //change in UI in all place if counter variable available
-=======
-  let [counter, setcounter] = useState(15) //change in UI in all place if counter variable available
->>>>>>> c8978ecd185e7aaf4e007194e23f653dc2292b57
 
   //let counter = 15
 
   function addvalue(){
     //counter += 1
-<<<<<<< HEAD
-=======
-    console.log(counter)
->>>>>>> c8978ecd185e7aaf4e007194e23f653dc2292b57
     setcounter(counter + 1)
   }
 
